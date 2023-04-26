c310e97f5954bacd6cb550cba1fa40edafea191b
yorch-dev <jltm.dev@outlook.com>
2023-04-26 16:18:42 -0400

    evaluación sesión 4

---
+++ modulo_03/sesion_04/evaluacion/diccionario.py
@@ -0,0 +1,21 @@
+
+
+persona_1 = {
+    'nombre': 'nombre1',
+    'apellido': 'apellido1',
+    'telefono': '987654321'
+}
+persona_2 = {
+    'nombre': 'nombre2',
+    'apellido': 'apellido2',
+    'telefono': '987654322'
+}
+persona_3 = {
+    'nombre': 'nombre3',
+    'apellido': 'apellido3',
+    'telefono': '987654323'
+}
+
+lista_personas = [persona_1, persona_2, persona_3]
+
+print(lista_personas)
